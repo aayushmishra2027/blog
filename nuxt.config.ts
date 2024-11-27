2b6e37f76ca119bf8cf646a40fa382e7305a5991
Aayush Mishra <aayushmishra1105@gmail.com>
2024-11-28 00:25:31 +0530

    Create nuxt.config.ts

---
+++ nuxt.config.ts
@@ -0,0 +1,41 @@
+export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {},
+  },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxt/content",
+    "@nuxtjs/web-vitals",
+    "@nuxtjs/robots",
+  ],
+  extends: "@nuxt-themes/typography",
+  colorMode: {
+    preference: "dark",
+    classSuffix: "",
+  },
+  content: {
+    markdown: {
+      remarkPlugins: ["remark-reading-time"],
+    },
+    highlight: {
+      theme: {
+        default: "github-light",
+        dark: "github-dark",
+      },
+    },
+  },
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+  },
+  webVitals: {
+    provider: "log",
+    debug: true, // debug enable metrics reporting on dev environments
+    disabled: false,
+  },
+  nitro: {
+    prerender: {
+      routes: ["/sitemap.xml", "/rss.xml"],
+    },
+  },
+});
