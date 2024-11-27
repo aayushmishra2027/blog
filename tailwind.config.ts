a9101a0eba7a18cec9ace10fefaa74a2f191c198
Aayush Mishra <aayushmishra1105@gmail.com>
2024-11-28 00:18:07 +0530

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+//import defaultTheme from "@tailwindcss/defaultTheme";
+export default <Partial<Config>>{
+  darkMode: "class",
+  plugins: [require("@tailwindcss/typography")],
+  content: ["content/**/*.md"],
+  theme: {
+    extend: {
+      colors: {},
+      fontFamily: {
+        recoleta: ["Recoleta", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+};
